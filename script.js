df3b99564688313004b58833f65f91dc89d1c2c8
itsmedanb <itsmedanb@gmail.com>
2022-01-03 15:15:03 -0700

    redid logic behind game, added player input

--- script.js
+++ script.js
@@ -1,50 +1,73 @@
-
-let choice = "choice";
-let playerChoice = "rock";
+let choice = "";
+let roundWinner = "";
+let computerChoice = choice;
+let playerChoice = "";
 let playerScore = 0;
 let computerScore = 0;
 
-function computerPlay(){
-  
+
+
+let computerPlay = () => {
   switch(Math.floor(Math.random() *3)){
     case 0:
-      choice = "rock";
+      computerChoice = "rock";
       break;
     case 1:
-      choice = "paper";
+      computerChoice = "paper";
       break;
     case 2:
-      choice = "scissors";
+      computerChoice = "scissors";
       break;
   }
-  return choice;
-  
-}
+  return computerChoice;
+};
 
-function playRound (){
-  if(playerChoice == "rock" && computerPlay() == "scissors" || playerChoice == "paper" && computerPlay() == "rock" || playerChoice == "scissors" && computerPlay() == "paper"){
-    return "you win";
+let playRound = () => {
+  computerPlay();
+  if(playerChoice == "rock" && computerChoice == "rock" || playerChoice == "paper" && computerChoice == "paper" || playerChoice == "scissors" && computerChoice == "scissors"){
+    roundWinner = "tie";
+  } 
+  else if(playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper"){
+    roundWinner = "player";
   }
-  else if(computerPlay() == " rock" && playerChoice == "scissors" || computerPlay() == "paper" && playerChoice == "rock" || computerPlay() == "scissors" && playerChoice == "paper"){
-    return "you lose";
+  else if(playerChoice == "scissors" && computerChoice == "rock" || playerChoice == "rock" && computerChoice == "paper" || playerChoice == "paper" && computerChoice == "scissors"){
+    roundWinner = "computer";
   }
   else{
-    return "tie";
+    console.log("error, playerChoice is not a correct answer");
   }
-}
+};
 
-function game(){
-  playRound(playerChoice, computerPlay());
-  
-  if(playRound() == "you win"){
-    playerScore++;
+let game = () => {
+do{
+  playerChoice = prompt("choice");
+  playRound();
+  if(roundWinner == "tie"){
+    console.log("tie, no one won.");
   }
-  if (playRound() == "you lose"){
+  else if(roundWinner == "player"){
+    console.log("You won the round");
+    playerScore++;
+  }else if (roundWinner == "computer"){
+    console.log("The computer won the round");
     computerScore++;
   }
-  console.log(playerScore, computerScore);
+  else{
+    console.log("stopping game");
+    break;
+  }
+  console.log("player score is " + playerScore + " computerScore " + computerScore);
+}
+while (playerScore < 5 && computerScore < 5);
+
+if(playerScore == 5){
+  console.log("Player wins the game");
+}
+else if (computerScore == 5){
+  console.log("Computer wins the game");
 }
 
-while (playerScore < 4 || computerScore < 4) {
-  game();
-}
+} 
+
+
+game();
